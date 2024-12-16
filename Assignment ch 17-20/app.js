9d6a99e01bd3315acf2de6afee204ee7a2a7b93f
MEHAK MEHTAB <mehakmehtab01@gmail.com>
2024-12-16 10:39:08 +0500

    Add files via upload

---
+++ Assignment ch 17-20/app.js
@@ -0,0 +1,100 @@
+// Assignment ch 17-20;
+// ARRAYS AND LOOP;
+
+// Question no 01:
+// let arr = [
+//     menu1=[],
+//     meun2=[],
+//     menu3=[],
+// ]
+
+// Question no 2:
+// let arr = [
+//    [0,1,2,3],
+//    [1,0,1,2],
+//    [2,1,0,1],
+// ]
+// document.write(arr);
+
+
+// Question no 3:
+// for(let i =0; i<=10;i++){
+//     document.write(i + "<br>")
+// }
+
+// Question no 4:
+// let table = Number(prompt("please enter the table number"));
+// let tablelength =Number( prompt("please enter the table length"));
+
+// for(let i=0; i<=tablelength;i++){
+//     document.write(table + " X " + i + " = " + table*i + " <br>")
+// }
+
+
+//  Question no 5;
+// let fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+// for(let i=0; i<fruits.length; i++){
+//     document.write("Element at index" )
+// }
+
+
+// Question no 6:
+// for(let i=1;i<=15;i++){
+//     document.write(i + " <br>");
+// }
+
+// for (let i = 10; i >= 1; i--) {
+//     document.write(i + " <br>");
+// }
+
+// for(let i=0; i<=20; i++){
+//     if(i % 2 == 0){
+       
+//         document.write( i+ " <br>");
+//     }
+//     else{
+//         document.write("")
+//     }
+// }
+
+// for(let i=0; i<=20; i++){
+//     if(i % 2 == 0){
+       
+//         document.write("");
+//     }
+//     else{
+//         document.write(i+ " <br>")
+//     }
+// }
+
+// for(let i=2; i<=20; i++){
+//     if(i % 2 == 0){
+       
+//         document.write( i+"K" + " <br>");
+//     }
+//     else{
+//         document.write("")
+//     }
+// }
+
+// Question no 07:
+
+// let A = ["cake", "apple pie", "cookie", "chips", "patties"];
+// let order = prompt("Welcome to ABC Bakery. What do you want to order Sir/ma'am?");
+
+// for(let i=0; i <A.length;i++){
+// if(order == A[i]){
+//     document.write(order + " is present at "+ i);
+//     break;
+// }
+// else{
+//     document.write("Sorry it's not available ");
+//     break;
+// }
+// }
+
+// Question no 08 :
+let arr = [24, 53, 78, 91, 12];
+
+
